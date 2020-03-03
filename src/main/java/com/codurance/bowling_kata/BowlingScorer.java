cd9af0eb1345f9a7b19ab9d184f12149c993f6ee
Tom Spencer <tom@homeflow.co.uk>
2020-03-03 16:13:11 +0000

    Add rule of three to implement naive solution for one in three consecutive frames

--- src/main/java/com/codurance/bowling_kata/BowlingScorer.java
+++ src/main/java/com/codurance/bowling_kata/BowlingScorer.java
@@ -2,6 +2,12 @@ package com.codurance.bowling_kata;
 
 public class BowlingScorer {
   public int roll(String score) {
+    if (score.equals("--|--|1-|--|--|--|--|--|--|--||"))
+      return 1;
+    if (score.equals("--|1-|--|--|--|--|--|--|--|--||"))
+      return 1;
+    if (score.equals("1-|--|--|--|--|--|--|--|--|--||"))
+      return 1;
     return 0;
   }
 }
--- src/test/java/com/codurance/bowling_kata/BowlingScorerShould.java
+++ src/test/java/com/codurance/bowling_kata/BowlingScorerShould.java
@@ -7,9 +7,32 @@ public class BowlingScorerShould {
   @Test
   public void return_zero_from_zero() {
     BowlingScorer bowlingScorer = new BowlingScorer();
-    String score = "-";
+    String score = "--|--|--|--|--|--|--|--|--|--||";
     int roll = bowlingScorer.roll(score);
     Assert.assertEquals(0, roll);
+  }
 
+  @Test
+  public void return_one_from_one() {
+    BowlingScorer bowlingScorer = new BowlingScorer();
+    String score = "1-|--|--|--|--|--|--|--|--|--||";
+    int roll = bowlingScorer.roll(score);
+    Assert.assertEquals(1, roll);
+  }
+
+  @Test
+  public void return_one_from_one_in_the_second_frame() {
+    BowlingScorer bowlingScorer = new BowlingScorer();
+    String score = "--|1-|--|--|--|--|--|--|--|--||";
+    int roll = bowlingScorer.roll(score);
+    Assert.assertEquals(1, roll);
+  }
+
+  @Test
+  public void return_one_from_one_in_the_third_frame() {
+    BowlingScorer bowlingScorer = new BowlingScorer();
+    String score = "--|--|1-|--|--|--|--|--|--|--||";
+    int roll = bowlingScorer.roll(score);
+    Assert.assertEquals(1, roll);
   }
 }
