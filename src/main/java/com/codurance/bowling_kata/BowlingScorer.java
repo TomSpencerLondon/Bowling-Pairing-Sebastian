6b29c5d48d152caca7c39fb1f787637158c5dbbd
Tom Spencer <tom@homeflow.co.uk>
2020-03-04 09:01:14 +0000

    Add extra code for BowlingScorer with stream()

--- src/main/java/com/codurance/bowling_kata/BowlingScorer.java
+++ src/main/java/com/codurance/bowling_kata/BowlingScorer.java
@@ -5,14 +5,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class BowlingScorer {
+
   public int roll(String score) {
     int sum = 0;
-//    char[] rolls = score.toCharArray();
+    score = score.replaceAll("-", "0");
     List<Character> rolls = new ArrayList<>();
     for (char s : score.toCharArray()) {
       rolls.add(s);
     }
-    rolls = rolls.stream().filter(c -> c != '|').collect(Collectors.toList());
+    rolls = rolls.stream()
+                 .filter(c -> c != '|')
+                 .collect(Collectors.toList());
+
     for (char roll : rolls) {
       if (Character.isDigit(roll)){
         sum += Character.getNumericValue(roll);
